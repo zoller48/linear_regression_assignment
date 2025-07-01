7e629176aacf9016dfcaf1a9d030707df678f4ef
Kate Zoller <zoller48@ascend-rw01.ten.osc.edu>
2025-07-01 15:51:32 -0400

    calculated slope and correlation coefficient

--- .ipynb_checkpoints/linear_model_notebook-Copy1-checkpoint.r
+++ .ipynb_checkpoints/linear_model_notebook-Copy1-checkpoint.r
@@ -1,30 +1,6 @@
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) != 3) {
-  stop("Usage: Rscript linear_regression_r.R <filename> <x_column> <y_column>")
-}
-
-filename <- args[1]
-x_col <- args[2]
-y_col <- args[3]
-
-data <- read.csv(filename)
-formula <- as.formula(paste(y_col, "~", x_col))
-model <- lm(formula, data = data)
-
-library(ggplot2)
-plot <- ggplot(data, aes_string(x = x_col, y = y_col)) +
-  geom_point(color = "red") +
-  geom_smooth(method = "lm", color = "blue") +
-  ggtitle(paste(y_col, "vs", x_col)) +
-  xlab(x_col) +
-  ylab(y_col)
-
-ggsave("linear_regression_r_output.png", plot)
-print(plot)
-
 #read csv file thank u 
 
-dataset <- read.csv("./regression_data.csv")
+dataset <- read.csv("regression_data.csv")
 
 #create scatterplot from the dataset 
 
@@ -48,7 +24,7 @@ library(ggplot2)
 
 #losing my mind haha
 
-#install.packages("ggplot2")
+install.packages("ggplot2")
 
 #please work
 
@@ -56,7 +32,7 @@ library(ggplot2)
 
 #enables advanced plotting but its not working 
 
-ggplot() +
+ggplot(regression_data.csv) +
   geom_point(aes(x = dataset$YearsExperience, y = dataset$Salary), colour = 'red') +
   geom_line(aes(x = dataset$YearsExperience, y = predict(model, newdata = dataset)), colour = 'blue') +
   ggtitle('Salary vs Experience') +
@@ -67,7 +43,7 @@ ggplot() +
 
 #oh because I renamed the dataset file to be just dataset
 
-ggplot(dataset) +
+ggplot2(dataset) +
   geom_point(aes(x = dataset$YearsExperience, y = dataset$Salary), colour = 'red') +
   geom_line(aes(x = dataset$YearsExperience, y = predict(model, newdata = dataset)), colour = 'blue') +
   ggtitle('Salary vs Experience') +
@@ -77,7 +53,7 @@ ggplot(dataset) +
 
 #okay thought i had already installed ggplot2
 
-#install.packages("ggplot2")
+install.packages("ggplot2")
 
 
 #okay it appears as though that worked what a win for me 
@@ -136,6 +112,41 @@ ggplot() +
 summary(model)
 
 #R complete dang that was a wild ride 
+
+
+#renaming my files to be in the correct notation for assignment
+
+library(ggplot2)
+
+# Data
+df <- data.frame(
+  x = c(1, 2, 3, 4, 5),
+  y = c(2.1, 4.3, 6.1, 8.0, 10.1)
+)
+
+# Fit model
+model <- lm(y ~ x, data = df)
+slope <- coef(model)[2]
+intercept <- coef(model)[1]
+r <- cor(df$x, df$y)
+pred <- predict(model)
+mse <- mean((df$y - pred)^2)
+
+# Plot
+ggplot(df, aes(x = x, y = y)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  annotate("text", x = 1.5, y = max(df$y) - 0.5,
+           label = paste("y =", round(slope, 2), "x +", round(intercept, 2),
+                         "\nr =", round(r, 2), "\nMSE =", round(mse, 2)),
+           size = 4) +
+  labs(title = "Linear Fit",
+       x = "Years of Experience", y = "Salary") +
+  theme_minimal()
+
+ggsave("regression_plot_r.png")
+
+debug
 
 
 
--- .ipynb_checkpoints/linear_model_notebook-checkpoint.py
+++ .ipynb_checkpoints/linear_model_notebook-checkpoint.py
@@ -2,6 +2,8 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from scipy.stats import linregress
+import numpy as np 
 
 if len(sys.argv) != 4:
     print("Usage: python linear_regression_python.py <filename> <x_column> <y_column>")
@@ -12,15 +14,20 @@ x_col = sys.argv[2]
 y_col = sys.argv[3]
 
 data = pd.read_csv(filename)
+x = data[[x_col]].to_numpy().flatten
+y = data[[y_col]].to_numpy().flatten
+
 model = LinearRegression()
 model.fit(data[[x_col]], data[[y_col]])
-
+slope, intercept, r_value, p_value, std_err = linregress(x_col, y_col)
+y_pred = slope * x + intercept
+mse = mean_squared_error(y, y_pred)
 plt.scatter(data[[x_col]], data[[y_col]], color='red')
 plt.plot(data[[x_col]], model.predict(data[[x_col]]), color='blue')
 plt.title(f'{y_col} vs {x_col}')
 plt.xlabel(x_col)
 plt.ylabel(y_col)
-plt.savefig("linear_regression_python_output.png")
+plt.savefig("linear_regression_python_.png")
 plt.show()
 
 #!/usr/bin/env python
